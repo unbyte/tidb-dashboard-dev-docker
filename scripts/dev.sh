ccf5d6e8d4835d5d433da1d4a147ede51d1635fd
Helios <i@shangyes.net>
2020-09-30 17:36:24 +0800

    feat: launch ui independently

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,6 +1,21 @@
+#!/bin/bash
+
+ui=""
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -u|--ui) ui="true" ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
 cd /workspace &&\
    make server &&\
    nohup bin/tidb-dashboard --debug --experimental --host 0.0.0.0 &
-cd ui &&\
-   yarn &&\
-   yarn start
+
+if [[ $ui = "true" ]]; then
+   cd ui &&\
+      yarn &&\
+      yarn start
+fi
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 source /root/.bash_profile
 tiup playground --host 0.0.0.0 $*
